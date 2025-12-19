8fabc35727842af7bb61a9fd03904a96ac4489bd
optionalaccc12-oss <optionalaccc12@gmail.com>
2025-12-19 18:01:22 +0530

    update

--- project.js
+++ project.js
@@ -1,13 +1,9 @@
-// This function runs when button is clicked
+
 function convert() {
 
-    // Get value from input box
     let c = document.getElementById("celsius").value;
-
-    // Convert Celsius to Fahrenheit
     let f = (c * 9 / 5) + 32;
 
-    // Show result on the page
     document.getElementById("result").innerHTML =
         "Fahrenheit: " + f;
 }
