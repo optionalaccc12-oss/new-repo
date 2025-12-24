9cf38f23a093264321ea7fb48f20ad9d5f5bf411
optionalaccc12-oss <optionalaccc12@gmail.com>
2025-12-24 16:56:06 +0530

    update

--- project.js
+++ project.js
@@ -1,9 +1,16 @@
+function setClock() {
+            const now = new Date();
+            const seconds = now.getSeconds();
+            const minutes = now.getMinutes();
+            const hours = now.getHours();
+            const secondDeg = ((seconds / 60) * 360) + 90;
+            const minuteDeg = ((minutes / 60) * 360) + 90;
+            const hourDeg = ((hours / 12) * 360) + 90;
 
-function convert() {
+            document.getElementById("second-hand").style.transform = `rotate(${secondDeg}deg)`;
+            document.getElementById("minute-hand").style.transform = `rotate(${minuteDeg}deg)`;
+            document.getElementById("hour-hand").style.transform = `rotate(${hourDeg}deg)`;
+        }
 
-    let c = document.getElementById("celsius").value;
-    let f = (c * 9 / 5) + 32;
-
-    document.getElementById("result").innerHTML =
-        "Fahrenheit: " + f;
-}
+        setInterval(setClock, 1000);
+        setClock();
